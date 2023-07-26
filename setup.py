edf0c3ffc989366e729b06d1b6dfb3635722de07
Jordi Aguilar Larruy <jalarruy@axt.email>
2023-07-26 10:31:54 +0200

    Add version in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ def main():
     """Main method collecting all the parameters to setup."""
     name = "sardana-albaem"
 
+    version = "0.0.14"
+
     description = "AlbaEM Sardana Controller"
 
     author = "kits"
